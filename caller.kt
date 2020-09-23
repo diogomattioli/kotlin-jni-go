3cac9aef40927d69f6d8bbe2ce04a79ab6ad8a04
Diogo Mattioli <diogomattioli@gmail.com>
2020-09-24 01:55:24 +0200

    Moving library load

--- caller.kt
+++ caller.kt
@@ -1,13 +1,11 @@
 class Foo {
-    init {
-        System.loadLibrary("foo")
-    }
     external fun add(a: Int, b: Int): Int
 }
 
 external fun mul(a: Int, b: Int): Int
 
 fun main() {
+    System.loadLibrary("foo")
     val c = Foo().add(1, 2)
     println(c)
     val d = mul(3, 4)
